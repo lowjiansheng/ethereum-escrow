b445a5f8d8f5b09033a3734455c4675d2a67bd77
lowjiansheng <lowjiansheng@hotmail.sg>
2020-12-06 15:07:39 +0800

    add web3 js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Web3 from 'web3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    console.log("network:", network)
+    
+    // Fetch account
+    const accounts = await web3.eth.getAccounts()
+    const etherBalance = await web3.eth.getBalance(accounts[0])
+    this.setState({
+      account: accounts[0],
+      balance: web3.utils.fromWei(etherBalance) 
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { 
+      account: '',
+      balance: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+          Account: {this.state.account}
+          </p>
+          <p>
+            Ether balance: {this.state.balance}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
